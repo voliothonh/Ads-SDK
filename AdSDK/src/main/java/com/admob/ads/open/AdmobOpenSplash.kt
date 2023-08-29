2c46aad4d311802e5f7423ddd71c6417a6de8b33
voliothonh <thonh@volio.vn>
2023-08-29 14:45:18 +0700

    Load high floor for Open Splash

--- AdSDK/src/main/java/com/admob/ads/open/AdmobOpenSplash.kt
+++ AdSDK/src/main/java/com/admob/ads/open/AdmobOpenSplash.kt
@@ -5,12 +5,13 @@ import android.os.CountDownTimer
 import com.admob.AdType
 import com.admob.TAdCallback
 import com.admob.ads.AdsSDK
-import com.admob.ads.interstitial.AdmobInterSplash
 import com.admob.getAppCompatActivityOnTop
 import com.admob.onNextActionWhenResume
 import com.admob.waitActivityResumed
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.appopen.AppOpenAd
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 object AdmobOpenSplash {
 
@@ -26,14 +27,14 @@ object AdmobOpenSplash {
         adUnitId: String,
         timeout: Long,
         onAdLoaded: () -> Unit = {},
-        onPaidValueListener : (Bundle) -> Unit,
+        onPaidValueListener: (Bundle) -> Unit,
         nextAction: () -> Unit
     ) {
 
         var isNextActionExecuted = false
 
-        fun callNextAction(){
-            if (isNextActionExecuted){
+        fun callNextAction() {
+            if (isNextActionExecuted) {
                 isNextActionExecuted = true
                 return
             }
@@ -81,6 +82,137 @@ object AdmobOpenSplash {
 
         AdmobOpen.load(
             adUnitId,
+            callback,
+            onAdLoaded = { adOpenAd = it }
+        )
+
+        timer?.cancel()
+        timer = object : CountDownTimer(timeout, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                if (!AdsSDK.isEnableOpenAds) {
+                    timer?.cancel()
+                    onAdLoaded.invoke()
+                    nextAction.invoke()
+                    return
+                }
+
+                adOpenAd?.let { appOpenAd ->
+                    timer?.cancel()
+                    onNextActionWhenResume {
+                        AdsSDK.getAppCompatActivityOnTop()?.waitActivityResumed {
+                            AdmobOpen.show(appOpenAd, callback)
+                        }
+                    }
+                }
+            }
+
+            override fun onFinish() {
+                timer?.cancel()
+                callNextAction()
+            }
+        }.start()
+    }
+
+    /**
+     * @param adUnitId: adUnitId
+     * @param timeout: timeout to wait ad show
+     * @param onAdLoaded: callback when adLoaded => for update UI or something
+     * @param nextAction: callback for your work, general handle nextActivity or nextFragment
+     */
+    fun showWithFloor(
+        adUnitIdH: String,
+        adUnitIdM: String,
+        adUnitIdL: String,
+        timeout: Long,
+        onAdLoaded: () -> Unit = {},
+        onPaidValueListener: (Bundle) -> Unit,
+        nextAction: () -> Unit
+    ) {
+
+        Firebase.analytics.logEvent("Splash_Ads_Floor", null)
+
+        var isNextActionExecuted = false
+
+        var currentAdUnitLoading = adUnitIdH
+
+        var adOpenAd: AppOpenAd? = null
+
+        fun callNextAction() {
+            if (isNextActionExecuted) {
+                isNextActionExecuted = true
+                return
+            }
+            onNextActionWhenResume(nextAction)
+        }
+
+        if (!AdsSDK.isEnableOpenAds) {
+            onAdLoaded.invoke()
+            nextAction.invoke()
+            return
+        }
+
+        val callback = object : TAdCallback {
+
+            override fun onAdLoaded(adUnit: String, adType: AdType) {
+                super.onAdLoaded(adUnit, adType)
+                onAdLoaded.invoke()
+            }
+
+            override fun onAdFailedToLoad(adUnit: String, adType: AdType, error: LoadAdError) {
+                super.onAdFailedToLoad(adUnit, adType, error)
+
+
+                currentAdUnitLoading = when (currentAdUnitLoading) {
+                    adUnitIdH -> {
+                        Firebase.analytics.logEvent("Splash_Ads_Floor_Medium", null)
+                        adUnitIdM
+                    }
+
+                    adUnitIdM -> {
+                        Firebase.analytics.logEvent("Splash_Ads_Floor_Low", null)
+                        adUnitIdL
+                    }
+
+                    else -> {
+                        ""
+                    }
+                }
+
+                if (currentAdUnitLoading.isEmpty()) {
+                    timer?.cancel()
+                    callNextAction()
+                } else {
+                    AdmobOpen.load(
+                        currentAdUnitLoading,
+                        this,
+                        onAdLoaded = { adOpenAd = it }
+                    )
+                }
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adUnit: String, adType: AdType) {
+                super.onAdFailedToShowFullScreenContent(adUnit, adType)
+                timer?.cancel()
+                callNextAction()
+            }
+
+            override fun onAdDismissedFullScreenContent(adUnit: String, adType: AdType) {
+                super.onAdDismissedFullScreenContent(adUnit, adType)
+                timer?.cancel()
+                callNextAction()
+            }
+
+            override fun onPaidValueListener(bundle: Bundle) {
+                super.onPaidValueListener(bundle)
+                onPaidValueListener.invoke(bundle)
+            }
+        }
+
+        Firebase.analytics.logEvent("Splash_Ads_Floor_High", null)
+
+        AdmobOpen.load(
+            currentAdUnitLoading,
             callback,
             onAdLoaded = { adOpenAd = it }
         )
