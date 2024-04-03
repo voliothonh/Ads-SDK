144da61d743f07f83f6b536bc9c3eb77147d1984
DucLH <duclh@volio.vn>
2024-04-03 13:54:02 +0700

    fix dialog reward

--- AdSDK/src/main/java/com/admob/ui/dialogs/DialogShowLoadingAds.kt
+++ AdSDK/src/main/java/com/admob/ui/dialogs/DialogShowLoadingAds.kt
@@ -2,10 +2,18 @@ package com.admob.ui.dialogs
 
 import android.content.Context
 import android.view.LayoutInflater
-import com.admob.ads.databinding.DialogLoadingInterBinding
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.admob.ads.R
 import com.admob.ui.BaseDialog
 
-class DialogShowLoadingAds(context: Context) : BaseDialog<DialogLoadingInterBinding>(context) {
-    override val binding = DialogLoadingInterBinding.inflate(LayoutInflater.from(context))
+class DialogShowLoadingAds(context: Context) : BaseDialog<ViewDataBinding>(context) {
+    override val binding: ViewDataBinding = DataBindingUtil.inflate<ViewDataBinding>(
+        LayoutInflater.from(context),
+        R.layout.dialog_loading_inter,
+        null,
+        false
+    )
+
     override fun onViewReady() {}
 }
